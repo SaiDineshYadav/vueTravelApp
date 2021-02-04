e4d1e9231521211aca96bbc8031b2b3f157d5e27
SaiDineshYadav <boddusaidineshyadav@gmail.com>
2021-02-04 13:33:54 +0530

    Added routes example

---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav id="nav">
+      <ul class="nav-links">
+          <li class="links">
+      <router-link to="/"> Home </router-link>
+          </li>
+          <li v-for="destination of destinations" :key="destination.id" class="links">
+            <router-link :to="{name: 'DestinationDetails', params: {id: destination.id}}"> 
+                {{ destination.name }}
+            </router-link>
+          </li>
+      </ul>
+  </nav>
+</template>
+
+<style scoped>
+
+#nav {
+  padding: 30px;
+   display: flex;
+    align-items: center;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  padding: 0 10px;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
+
+<script>
+import store from '@/store.js';
+
+export default {
+     data() {
+    return {
+      destinations: store.destinations
+    }
+  }
+}
+</script>
---
+++ src/views/DestinationDetails.vue
@@ -0,0 +1,32 @@
+<template>
+   <section>
+       <h1>{{ destination.name }}</h1>
+       <div>
+           <img :src="require(`@/assets/${destination.image}`)" />
+           <p> {{destination.description}} </p>
+       </div>
+   </section>
+</template>
+
+<script>
+import store from '@/store.js';
+export default {
+    data() {
+        return {
+            destinationId: this.$route.params.id,
+        }
+    },
+    computed: {
+        destination() {
+            console.log()
+            return store.destinations.find(destinaton => destinaton.id == this.destinationId);
+        }
+    },
+    created() {
+        // console.log(this.$route.params.id)
+        // if (store.destinations.find(destinaton => destinaton.id == this.destinationId) == undefined) {
+        //     this.$route.push('about');
+        // }
+    }
+}
+</script>
